8f2d511137d8146974121e589a770b8461cda99a
hcorta <corta.hugo@gmail.com>
2023-07-06 12:40:16 +0200

    [green]: should return FizzBuzz for multiples of both 5 and 3

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,6 +3,10 @@ export function fizzbuzz(n: number): string {
     throw new Error('n must be between 1 and 100')
   }
 
+  if (n % 3 === 0 && n % 5 === 0) {
+    return 'FizzBuzz'
+  }
+
   if (n % 3 === 0) {
     return 'Fizz'
   }
