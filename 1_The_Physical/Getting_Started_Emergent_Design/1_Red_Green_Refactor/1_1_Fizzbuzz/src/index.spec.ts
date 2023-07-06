dc51f4e10be44038af28c09cd09a7d7717877e36
hcorta <corta.hugo@gmail.com>
2023-07-06 12:03:19 +0200

    [red]: should admit a number a return it as a string

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,4 +8,8 @@ describe('fizzbuzz', () => {
   it('should return a string', () => {
     expect(typeof fizzbuzz()).toEqual('string')
   })
+
+  it('should admit a number a return it as a string', () => {
+    expect(fizzbuzz(1)).toEqual('1')
+  })
 })
