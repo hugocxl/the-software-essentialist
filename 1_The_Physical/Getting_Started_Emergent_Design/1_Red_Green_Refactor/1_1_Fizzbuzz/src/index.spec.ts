5865da270bb0cfbe7da6fbac145f788d73a0c961
hcorta <corta.hugo@gmail.com>
2023-07-06 12:01:00 +0200

    [red]: it should return a string

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe('fizzbuzz', () => {
   it('should be defined', () => {
     expect(fizzbuzz).toBeDefined()
   })
+
+  it('should return a string', () => {
+    expect(typeof fizzbuzz()).toEqual('string')
+  })
 })
