c46a8554122907061b88e64ccb025b2468aaa3c2
hcorta <corta.hugo@gmail.com>
2023-07-07 11:38:18 +0200

    [green]: it should return true when checking Was It A Rat I Saw

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,6 @@
 export class PalindromeChecker {
-  public static check (str: string) {
-    const lowerCaseStr = str.toLowerCase()
+  public static check(str: string) {
+    const lowerCaseStr = str.toLowerCase().replace(/\s/g, '')
 
     if (lowerCaseStr.split('').reverse().join('') === lowerCaseStr) return true
 
