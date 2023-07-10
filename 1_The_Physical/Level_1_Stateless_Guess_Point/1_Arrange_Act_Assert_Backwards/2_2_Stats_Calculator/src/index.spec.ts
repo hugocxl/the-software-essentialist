eb326471b7fa897a32a7bbd94a30bcda859f313d
hcorta <corta.hugo@gmail.com>
2023-07-10 17:55:03 +0200

    [refactor]: review tests to avoid code duplication

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,36 +2,55 @@ import { StatsCalculator } from './index'
 
 describe('StatsCalculator', () => {
   describe('knows how to find the min value in a serie', () => {
-    it('should output "1" when the serie is [1, 2, 3, 4]', () => {
-      const output = StatsCalculator.calculate([1, 2, 3, 4])
+    it.each([
+      { input: [1, 2, 3, 4], expected: 1 },
+      { input: [11, 6, 31, 4], expected: 4 },
+    ])(
+      'should output $expected when the serie is $input',
+      ({ input, expected }) => {
+        const output = StatsCalculator.calculate(input)
 
-      expect(output.min).toEqual(1)
-    })
-    it('should output "4" when the serie is [11, 6, 31, 4]', () => {
-      const output = StatsCalculator.calculate([11, 6, 31, 4])
-
-      expect(output.min).toEqual(4)
-    })
+        expect(output.min).toEqual(expected)
+      }
+    )
   })
   describe('knows how to find the max value in a serie', () => {
-    it('should output "4" when the serie is [1, 2, 3, 4]', () => {
-      const output = StatsCalculator.calculate([1, 2, 3, 4])
+    it.each([
+      { input: [1, 2, 3, 4], expected: 4 },
+      { input: [11, 6, 31, 4], expected: 31 },
+    ])(
+      'should output $expected when the serie is $input',
+      ({ input, expected }) => {
+        const output = StatsCalculator.calculate(input)
 
-      expect(output.max).toEqual(4)
-    })
+        expect(output.max).toEqual(expected)
+      }
+    )
   })
   describe('knows how to calculate the total number of elements in a serie', () => {
-    it('should output "4" when the serie is [1, 2, 3, 4]', () => {
-      const output = StatsCalculator.calculate([1, 2, 3, 4])
+    it.each([
+      { input: [9, 1, 6, 3, 4], expected: 5 },
+      { input: [6, 31, 4], expected: 3 },
+    ])(
+      'should output $expected when the serie is $input',
+      ({ input, expected }) => {
+        const output = StatsCalculator.calculate(input)
 
-      expect(output.count).toEqual(4)
-    })
+        expect(output.count).toEqual(expected)
+      }
+    )
   })
   describe('knows how to calculate the average in a serie', () => {
-    it('should output "4" when the serie is [1, 2, 3, 4]', () => {
-      const output = StatsCalculator.calculate([1, 2, 3, 4])
+    it.each([
+      { input: [1, 2, 3, 4], expected: 2.5 },
+      { input: [11, 6, 31, 4], expected: 13 },
+    ])(
+      'should output $expected when the serie is $input',
+      ({ input, expected }) => {
+        const output = StatsCalculator.calculate(input)
 
-      expect(output.average).toEqual(2.5)
-    })
+        expect(output.average).toEqual(expected)
+      }
+    )
   })
 })
