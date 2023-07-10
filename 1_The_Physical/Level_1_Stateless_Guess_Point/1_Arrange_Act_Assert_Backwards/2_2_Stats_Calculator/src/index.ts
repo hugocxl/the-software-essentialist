5b98680eadd2cf003211c011cdd6034114585ff5
hcorta <corta.hugo@gmail.com>
2023-07-10 17:39:08 +0200

    [green]: should output 4 when the serie is [11, 6, 31, 4]

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,7 +1,11 @@
 export class StatsCalculator {
   public static calculate(sequence: number[]) {
+    const min = sequence.reduce((min, current) =>
+      current < min ? current : min
+    )
+
     return {
-      min: 1,
+      min,
       max: 0,
       average: 0,
       count: 0,
