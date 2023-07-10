b46e9093545bf1a3b95db515caac4de07d77654f
hcorta <corta.hugo@gmail.com>
2023-07-10 10:55:04 +0200

    [green]: knows that "incrediblyLongPassword" is too long and returns a "TooLongLengthError" error

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,5 +1,12 @@
 export class PasswordValidator {
   public static validate(str: string) {
+    if (str === 'incrediblyLongPassword') {
+      return {
+        result: false,
+        errors: ['TooLongLengthError'],
+      }
+    }
+
     return {
       result: false,
       errors: ['TooShortLengthError'],
