fbd55e4633023a160aa6721aabfaf1efad92bc89
hcorta <corta.hugo@gmail.com>
2023-07-10 17:58:54 +0200

    [refactor]: extracted methods in StatsCalculator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,15 +1,21 @@
 export class StatsCalculator {
+  private static getMin(sequence: number[]) {
+    return sequence.reduce((min, current) => (current < min ? current : min))
+  }
+
+  private static getMax(sequence: number[]) {
+    return sequence.reduce((max, current) => (current > max ? current : max))
+  }
+
+  private static getAverage(sequence: number[]) {
+    return sequence.reduce((sum, current) => sum + current) / sequence.length
+  }
+
   public static calculate(sequence: number[]) {
-    const min = sequence.reduce((min, current) =>
-      current < min ? current : min
-    )
-    const max = sequence.reduce((max, current) =>
-      current > max ? current : max
-    )
-
+    const min = this.getMin(sequence)
+    const max = this.getMax(sequence)
+    const average = this.getAverage(sequence)
     const count = sequence.length
-
-    const average = sequence.reduce((sum, current) => sum + current) / count
 
     return {
       min,
